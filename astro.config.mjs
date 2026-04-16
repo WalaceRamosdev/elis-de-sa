f339e0c6c13b8cdd4e39ee30ce34930302492e5d
Walace Ramos <contatowalace.dev@gmail.com>
2026-04-16 16:30:38 -0300

    Projeto Elis de Sá - Estrutura completa

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://elis-de-sa-enfermeira.com.br', // Placeholder, update if needed
+  output: 'static'
+});
